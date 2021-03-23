d4252d1716334a76b448f8e5672d27f65e61f15f
Sara Stankiewicz <sarastankiewicz@MacBook-Air.local>
2021-03-23 16:49:15 -0400

    changing the medata scrit  to create the metadata file from a dataframe

--- inst/scripts/make-metadata.R
+++ inst/scripts/make-metadata.R
@@ -1,4 +1,33 @@
 library(AnnotationHubData)
 
-AnnotationHubData::makeAnnotationHubMetadata(
-  "/Users/sarastankiewicz/workDir/gwascatData", fileName = "gwascatDataMetadata.csv" )
+#AnnotationHubData::makeAnnotationHubMetadata(
+ # "/Users/sarastankiewicz/workDir/gwascatData", fileName = "gwascatDataMetadata.csv" )
+
+
+
+#its the tags that are causing there to be two lines need to make the tags be in one column.
+tags = c("GWAS", "GWAS catalog")
+
+metadata <- data.frame(
+  Title = "gwascatData",
+  Description = "text file in cloud with Nov 1 2020 snapshot of EBI/EMBL GWAS catalog",
+  BiocVersion = "3.13",
+  Genome = "GRCh38",
+  SourceType = "TSV",
+  SourceUrl = "http://www.ebi.ac.uk/gwas/api/search/downloads/alternative",
+  SourceVersion = "11.1.2020",
+  Species = "Homo_Sapiens",
+  TaxonomyId = 9606 ,
+  Coordinate_1_based = TRUE,
+  DataProvider ="EBI/EMBL",
+  Maintainer = "Sara Stankiewicz <reshs@channing.harvard.edu>",
+  RDataClass = "character",
+  DispatchClass = "data.frame",
+  Location_Prefix = "http://s3.amazonaws.com/",
+  RDataPath = "biocfound-gwascat/gwascat_38_2020_11_01.tsv.gz",
+  #Tags = c("GWAS", "GWAS catalog")
+  Tags = "GWAS:GWAS catalog"
+)
+
+#getting the error non-numeric argument to binary operator
+write.csv(metadata, file - "metadataTest.csv", row.names = FALSE)
