23c52b29096e311f94c2d7023061538bfd6f34d1
Sara Stankiewicz <sarastankiewicz@MacBook-Air.fios-router.home>
2021-04-09 21:04:59 -0400

    fixing the script to depect the

--- inst/scripts/make-data.R
+++ inst/scripts/make-data.R
@@ -1,40 +1,18 @@
-# this is a proposed script to make the data resource that we want,
-# which is a data.frame derived from a snapshot of EBI GWAS catalog,
-# that we are currently hosting on AWS S3 funded by Bioconductor Foundation
+#The data for the gwascat package is a  snapshot of the GWAS catalog that can be
+#found on the National Human Genome Research Institute website. The code below
+#is the exact code used to get the snapshot done on March 30 2021. The first
+#line must be done in terminal.
 
-# script was prescribed by 
-# https://bioconductor.org/packages/release/bioc/vignettes/AnnotationHubData/inst/doc/CreateAnAnnotationPackage.html
-# but it is not clear how the script could use metadata fields to identify
-# what it will do
+#  wget http://www.ebi.ac.uk/gwas/api/search/downloads/alternative
 
-#here i need the code that was used to create/ download the data.
+# To get the snap shot of the GWAS data into R. Open R in the directory that the
+# file alternative was put in and run the following command. The YYYY_MM_DD is
+# the date the alternative file was downloaded.
 
-#table.url = paste([Location_prefix], [Rdatapath])
-library(readr)
-table.url = "http://s3.amazonaws.com/biocfound-gwascat/gwascat_38_2020_11_01.tsv.gz"
-tf = tempfile()
+library(data.table)
+gwascat_tmp = data.table::fread("alternative")
+gwascat_YYYY_MM_DD = as.data.table(gwascat_tmp)
 
- tst = try(download.file(table.url, destfile=tf))
- if (inherits(tst, "try-error")) stop("could not complete download")
+save(gwascat_YYYY_MM_DD, file="gwascat_YYYY_MM_DD.Rda", compress=xz)
 
- ct = readr::cols(
-  .default = col_character(),
-  `DATE ADDED TO CATALOG` = col_date(format = ""),
-  PUBMEDID = col_double(),
-  DATE = col_date(format = ""),
-  CHR_ID = col_character(),
-  CHR_POS = col_double(),
-  UPSTREAM_GENE_DISTANCE = col_double(),
-  DOWNSTREAM_GENE_DISTANCE = col_double(),
-  MERGED = col_double(),
-  SNP_ID_CURRENT = col_double(),
-  INTERGENIC = col_double(),
-  `P-VALUE` = col_double(),
-  PVALUE_MLOG = col_double(),
-  `OR or BETA` = col_double()
-)
-
- tab = readr::read_tsv(tf, col_types=ct) #, sep="\t", header=TRUE, check.names=FALSE, stringsAsFactors=FALSE)
- message(paste0("formatting gwaswloc instance..."))
- tab = as.data.frame(tab)
 
