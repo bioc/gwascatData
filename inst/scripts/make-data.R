5cbf674f7875132dae39d1b80afca59ea3aae79c
Sara Stankiewicz <sarastankiewicz@MacBook-Air.fios-router.home>
2021-04-09 21:16:55 -0400

    fixing minor grammer

--- inst/scripts/make-data.R
+++ inst/scripts/make-data.R
@@ -1,13 +1,17 @@
+
 #The data for the gwascat package is a  snapshot of the GWAS catalog that can be
 #found on the National Human Genome Research Institute website. The code below
 #is the exact code used to get the snapshot done on March 30 2021. The first
-#line must be done in terminal.
+#line must be done in the terminal.
+
 
 #  wget http://www.ebi.ac.uk/gwas/api/search/downloads/alternative
 
-# To get the snap shot of the GWAS data into R. Open R in the directory that the
-# file alternative was put in and run the following command. The YYYY_MM_DD is
-# the date the alternative file was downloaded.
+
+#To get the snapshot of the GWAS data into R. Open R in the directory that the
+#file alternative was put in. Run the following command. The YYYY_MM_DD is
+#the date the alternative file was downloaded.
+
 
 library(data.table)
 gwascat_tmp = data.table::fread("alternative")
